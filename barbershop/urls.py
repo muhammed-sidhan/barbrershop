2dbdec4d33dddbf26dc9338202ffa0db500028dc
muhammed-sidhan <77342194+muhammed-sidhan@users.noreply.github.com>
2025-07-28 04:13:56 +0530

    google seo(1)

--- barbershop/urls.py
+++ barbershop/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
-
+from django.views.generic import TemplateView
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('core.urls'))
+    path('',include('core.urls')),
+     # robots.txt served from template
+    path('robots.txt', TemplateView.as_view(template_name='robots.txt', content_type='text/plain')),
 ]
